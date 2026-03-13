84fdfc6f49e94e5e42d22d6ff36628b72fbfedad
Juan Sebastian Valencia <juan.quinchua@correounivalle.edu.co>
2026-03-13 18:22:40 -0500

    Iniciación de proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.miniproyecto_1_soleclipsado {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.miniproyecto_1_soleclipsado to javafx.fxml;
+    exports com.example.miniproyecto_1_soleclipsado;
+}
